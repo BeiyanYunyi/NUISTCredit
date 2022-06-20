86d5e983edc4367b27e440b960d84c69ba35239b
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-06-20 11:00:26 +0800

    📝 Add introduction

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -10,8 +10,20 @@ const About: Component = () => (
       <div class="mx-auto max-w-7xl py-6 px-6 lg:px-8">
         <Panel prose>
           <article>
-            <h1>🔐 School Credit</h1>
+            <h1>🔐 NUIST Credit</h1>
             <p>为你的小票打印机（咕咕机）生成一个可供打印的网页</p>
+            <h2>📝 说明</h2>
+            <p>有什么说明的必要吗？（恼）</p>
+            <ul>
+              <li>主页点击各项可删除</li>
+              <li>生成后的页面点击校徽可以返回主页</li>
+            </ul>
+            <p>
+              欢迎来 Star：
+              <a href="https://github.com/lixiang810/NUISTCredit" target="_blank">
+                <img class="inline !m-0 h-8 w-8" src={GhIcon} />
+              </a>
+            </p>
           </article>
         </Panel>
       </div>
